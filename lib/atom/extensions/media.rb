f694e1ed39d0cee07f0241808f5ea504eeee1912
Hưng Đào <zocoi@users.noreply.github.com>
2017-06-19 14:46:59 -0700

    Create media.rb

---
+++ lib/atom/extensions/media.rb
@@ -0,0 +1,50 @@
+require 'atom'
+
+#
+# Media extension (see http://video.search.yahoo.com/mrss)
+#
+# Exaple:
+#     f.entries << Atom::Entry.new do |e|
+#       e.media_content = Media.new :url => article.media['image']
+#     end
+#
+
+module Media
+  class Base
+      include Atom::Xml::Parseable
+      include Atom::SimpleExtensions
+
+      def initialize(o = {})
+        case o
+        when XML::Reader
+          parse(o, :once => true)
+        when Hash
+          o.each do |k, v|
+            self.send("#{k.to_s}=", v)
+          end
+        else
+          raise ArgumentError, "Got #{o.class} but expected a Hash or XML::Reader"
+        end
+
+        yield(self) if block_given?
+      end
+  end
+  
+  class Content < Base
+    attribute :url, :fileSize, :type, :medium, :isDefault, :expression, :bitrate, :height, :width, :duration, :lang
+  end
+  
+  class Location < Base
+    attribute :id, :type
+  end
+  
+  class Category < Base
+    attribute :name
+  end
+end
+
+Atom::Feed.add_extension_namespace :media, "http://search.yahoo.com/mrss/"
+Atom::Entry.element "media:content", class: Media::Content
+Atom::Entry.elements "media:location", class: Media::Location
+Atom::Entry.element "media:category", class: Media::Category
+
